4ff7d01814f8726bd85a576f27a9a9e9700fdf4f
hanjinhui638 <jh312jh@naver.com>
2019-11-07 17:22:16 +0900

    2019-11-07

--- src/test/java/com/jh/s1/DataSourceTest.java
+++ src/test/java/com/jh/s1/DataSourceTest.java
@@ -3,18 +3,18 @@ package com.jh.s1;
 import static org.junit.Assert.*;
 
 import javax.inject.Inject;
-import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 public class DataSourceTest extends TestAbstractCase{
 
 	@Inject
-	private DataSource dataSource;
+	private SqlSession sqlSession;
 	
 	@Test
 	public void test() throws Exception{
-		assertNotNull(dataSource.getConnection());
+		assertNotNull(sqlSession);
 	}
 
 }
